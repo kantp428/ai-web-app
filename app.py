6cd77a48d3048e13ffffdb0e037e301b6e0410d1
mrktp <misterkantapat@gmail.com>
2025-10-04 06:32:57 +0700

    fix app.py

--- app.py
+++ app.py
@@ -19,4 +19,7 @@ def predict():
     return jsonify(predictions)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render จะส่ง PORT มา
+    app.run(host="0.0.0.0", port=port, debug=False)
+
